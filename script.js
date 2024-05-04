004f400e5d7f5616da7afee10d1a6fe605a20cf0
Archie Productions Inc <archieproductionsinc@gmail.com>
2024-05-04 09:28:31 +0800

    Vanta JS

--- script.js
+++ script.js
@@ -49,4 +49,20 @@ window.onload = function() {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
+
+    VANTA.NET({
+        el: "#home-page",
+        mouseControls: false,
+        touchControls: false,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x0,
+        backgroundColor: 0x7d7d7d,
+        points: 20.00,
+        maxDistance: 13.00,
+        spacing: 13.00
+    })
 }
