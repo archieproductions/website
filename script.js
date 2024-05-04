6955f10cf874b384c755ea5ec3d935e84b88754c
Archie Productions Inc <archieproductionsinc@gmail.com>
2024-05-04 09:50:27 +0800

    Updated Files

--- script.js
+++ script.js
@@ -50,19 +50,20 @@ window.onload = function() {
         }
     }
 
+    // Vanta JS part
     VANTA.NET({
-        el: "#home-page",
+        el: "body",
         mouseControls: false,
         touchControls: false,
         gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
+        minHeight: 50.00,
+        minWidth: 50.00,
         scale: 1.00,
         scaleMobile: 1.00,
-        color: 0x0,
-        backgroundColor: 0x7d7d7d,
+        color: 0x525252,
+        backgroundColor: 0xfff9f3,
         points: 20.00,
-        maxDistance: 13.00,
-        spacing: 13.00
+        maxDistance: 15.00,
+        spacing: 15.00
     })
-}
+};
